51cf8591c32f9686e57bdb0e907b36f0a4840920
Rashed Mazumder <yaan.uchiha29@gmail.com>
2019-10-22 15:00:05 +0600

    readme

--- example/README.md
+++ example/README.md
@@ -14,3 +14,24 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+##### A flutter package for autocompleting suggestion.
+
+### Usage 
+
+
+
+### Example
+
+```dart
+ AutoSuggestionTextField(
+        textController: _nameTextController,
+        suggestionStrings: suggestionStrings,
+        onValueChanged: (dynamic newValue){
+          print("new Val - $newValue");
+        },
+),
+```
+
+
+
--- lib/suggestion_textfield.dart
+++ lib/suggestion_textfield.dart
@@ -1,4 +1,5 @@
 library suggestion_textfield;
+
 import 'package:flutter/material.dart';
 
 /// AutoSuggestion class
@@ -17,12 +18,15 @@ class AutoSuggestionTextField extends StatefulWidget {
 
   const AutoSuggestionTextField(
       {Key key,
-        @required this.textController,
-        @required this.suggestionStrings, this.onValueChanged, this.suggestionBoxDecoration})
+      @required this.textController,
+      @required this.suggestionStrings,
+      this.onValueChanged,
+      this.suggestionBoxDecoration})
       : super(key: key);
 
   @override
-  _AutoSuggestionTextFieldState createState() => _AutoSuggestionTextFieldState();
+  _AutoSuggestionTextFieldState createState() =>
+      _AutoSuggestionTextFieldState();
 }
 
 class _AutoSuggestionTextFieldState extends State<AutoSuggestionTextField> {
@@ -55,13 +59,12 @@ class _AutoSuggestionTextFieldState extends State<AutoSuggestionTextField> {
       // show items
       if (matchedItems.length > 0) {
         //////// Temporary fix for the bug when selecting string box pops up twice //////////
-        if(matchedItems.length==1 && matchedItems[0]==widget.textController.text){
+        if (matchedItems.length == 1 &&
+            matchedItems[0] == widget.textController.text) {
           showSuggestionBox = false;
-        }
-        else{
+        } else {
           showSuggestionBox = true;
         }
-
       } else {
         showSuggestionBox = false;
       }
@@ -85,8 +88,6 @@ class _AutoSuggestionTextFieldState extends State<AutoSuggestionTextField> {
 
       widget.onValueChanged(selectedItem);
     });
-
-
   }
 
   @override
@@ -109,34 +110,37 @@ class _AutoSuggestionTextFieldState extends State<AutoSuggestionTextField> {
           visible: showSuggestionBox,
           child: Container(
             padding: EdgeInsets.symmetric(vertical: 24, horizontal: 12),
-            decoration: widget.suggestionBoxDecoration ?? BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Colors.blue, width: 2),
-              borderRadius: BorderRadius.all(Radius.circular(12)),
-            ),
+            decoration: widget.suggestionBoxDecoration ??
+                BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.blue, width: 2),
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                ),
             child: ConstrainedBox(
               constraints: BoxConstraints(
                   maxWidth: MediaQuery.of(context).size.width,
-                  maxHeight: MediaQuery.of(context).size.height*0.2),
+                  maxHeight: MediaQuery.of(context).size.height * 0.2),
               child: ListView.builder(
                   shrinkWrap: true,
                   itemCount: matchedItems.length,
                   itemBuilder: (BuildContext context, int index) {
                     return Container(
                         child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            GestureDetector(
-                              onTap: () => _onItemTap(matchedItems[index]),
-                              child: Text(
-                                "${matchedItems[index]}",
-                                style: TextStyle(color: Colors.black, fontSize: 22),
-                              ),
-                            ),
-                            Divider(color: Colors.black54,),
-                          ],
-                        ));
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        GestureDetector(
+                          onTap: () => _onItemTap(matchedItems[index]),
+                          child: Text(
+                            "${matchedItems[index]}",
+                            style: TextStyle(color: Colors.black, fontSize: 22),
+                          ),
+                        ),
+                        Divider(
+                          color: Colors.black54,
+                        ),
+                      ],
+                    ));
                   }),
             ),
           ),
@@ -144,8 +148,4 @@ class _AutoSuggestionTextFieldState extends State<AutoSuggestionTextField> {
       ],
     );
   }
-
-
-
-
 }
